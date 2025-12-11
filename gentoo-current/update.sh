fe1c0f9485893151505320e32ec0c94293f4c4a9
Rukkhadevata123 <rukkhadevata@foxmail.com>
2025-12-12 00:43:38 +0800

    clang

--- gentoo-current/update.sh
+++ gentoo-current/update.sh
@@ -1,1 +1,2 @@
 sudo eix-sync && sudo emerge -auvDN @world && sudo emerge -ac  && sudo eclean-dist && sudo eclean-pkg && /var/db/repos/local/check_nv.sh && flatpak update && flatpak uninstall --unused && cd ~/.local/share/fcitx5/rime/ && git pull
+sudo apt update && sudo apt full-upgrade && sudo apt autoremove && sudo apt autoclean && cd ~/.config/ibus/rime/ && git pull && sudo spark-update-tool
